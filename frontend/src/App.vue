fe92debbd3b23df617bec221926df68f3f6cf295
Mohamed MALIJI <malijimohamed8@gmail.com>
2025-08-27 15:33:48 +0200

    feat: initialize Taskforce project

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<style>
+/* Base styles will be added later */
+</style>
