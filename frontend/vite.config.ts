68fa390c2e39c492f02fe01f8fe79d06b513ce70
Mohamed MALIJI <malijimohamed8@gmail.com>
2025-09-10 14:09:59 +0200

    ✨ feat: integrate Lexik JWT Authentication Bundle for enhanced security with JWT support, update environment configuration, and add necessary files for JWT key management

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,4 +15,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8003',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
